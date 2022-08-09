e47705272513c3cffb94adc17ac5437c9de6786f
JAMDev <jamdeveloper0@gmail.com>
2022-08-09 14:50:17 -0300

    Criação do menu do usuário

---
+++ src/Components/UserMenu/index.tsx
@@ -0,0 +1,63 @@
+import { Popover } from "@headlessui/react";
+import { useEffect, useState } from "react";
+import { PopoverButton, PopoverContainer, PopoverPanel, UserDataContainer } from "./styles";
+
+interface userData {
+  id: number
+  email: string
+  nome: string
+  data_nascimento: string
+  sexo: string
+  telefone: string
+  avatar: string
+}
+
+export function UserMenu() {
+  const [userData, setUserData] = useState<userData | null>(null)
+
+  useEffect(() => {
+    fetch('https://terraq.com.br/api/teste-leaflet/user')
+    .then((res) => res.json())
+    .then((data) => {
+      setUserData(data)
+    })
+  }, [])
+
+  return(
+    <PopoverContainer>
+      <PopoverButton>
+          Usuário
+      </PopoverButton>
+      <PopoverPanel>
+          {
+            userData
+            ? <>
+                <img src={userData.avatar} />
+                <UserDataContainer>
+                  <h2>
+                    {userData.nome}
+                  </h2>
+                  <div>
+                    <span>
+                      Nasceu em: {userData.data_nascimento}
+                    </span>
+                    <span>
+                      Sexo: {userData.sexo}
+                    </span>
+                    <span>
+                      Email: {userData.email}
+                    </span>
+                    <span>
+                      Telefone: {userData.telefone}
+                    </span>
+                  </div>
+                </UserDataContainer>
+              </>
+            : <span>
+                Loading...
+              </span>
+          }
+      </PopoverPanel>
+    </PopoverContainer>
+  )
+}
---
+++ src/Components/UserMenu/styles.ts
@@ -0,0 +1,70 @@
+import { Popover } from "@headlessui/react";
+import styled from "styled-components";
+
+export const PopoverContainer = styled(Popover)`
+    position: fixed;
+    left: 30px;
+    bottom: 30px;
+    z-index: 1000;
+`
+
+export const PopoverButton = styled(Popover.Button)`
+    border: none;
+    border-radius: 10px;
+
+    padding: 5px 10px;
+
+    width: fit-content;
+
+    background-color: #e02a2a;
+
+    color: aliceblue;
+    font-weight: 700;
+    font-size: 2rem;
+
+    cursor: pointer;
+`
+
+export const PopoverPanel = styled(Popover.Panel)`
+    position: absolute;
+    left: 0;
+    top: -16px;
+
+    border: 1.5px solid black;
+    border-radius: 2px;
+
+    padding: 10px;
+
+    transform: translateY(-100%);
+
+    display: flex;
+    column-gap: 1rem;
+
+    width: max-content;
+
+    max-width: 500px;
+
+    background-color: bisque;
+
+    img {
+        border: 2px solid black;
+        border-radius: 5px;
+
+        height: 100%;
+
+        aspect-ratio: 1/1;
+    }
+`
+
+export const UserDataContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    div {
+        display: flex;
+        flex-wrap: wrap;
+        column-gap: 0.5rem;
+        justify-content: space-around;
+    }
+`
